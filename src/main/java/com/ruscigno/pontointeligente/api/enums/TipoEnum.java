11574e77fbc6c59a348af22f1926df1871a68389
Sander Ruscigno <alessander.ruscigno@softplan.com.br>
2018-08-13 14:50:23 -0300

    Criação das classes das entidades

---
+++ src/main/java/com/ruscigno/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.ruscigno.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_AMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+}
