252d22bfdd4dba7e8b307c11b61256ddc4f6d152
Sander Ruscigno <alessander.ruscigno@softplan.com.br>
2018-08-15 18:16:45 -0300

    Criando os repositórios JPA - Parte 2/2

---
+++ src/main/java/com/ruscigno/pontointeligente/api/repositories/FuncionarioRepository.java
@@ -0,0 +1,15 @@
+package com.ruscigno.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ruscigno.pontointeligente.api.entities.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+	
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
+
+}
